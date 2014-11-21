cc38c118b116a45600e2fc816995f4d08ee95627
hanjiang387 <hanjiang387@gmail.com>
2014-11-21 10:38:02 +0800

    update

--- js/main.js
+++ js/main.js
@@ -9189,7 +9189,7 @@ return jQuery;
 
 }));
 
-
+console.log("new");
 
 function myDialog(option)
 {
@@ -9283,17 +9283,12 @@ function myDialog(option)
 			div_box.remove();
 		}
 	};
-};;var dialog;
-window.onload = function(){
-	document.getElementById("start").onclick = function(){
-		dialog = myDialog({
-							width:400,
-							height:200,
-							drag:true,
-							title:"请注意",
-							content:"这是个可以拖拽移动的弹出窗"
-							});
-		dialog.showDialog();
-	}
+};/*$(document).ready(function(){
+	alert(11);
+});*/
+$(document).ready(function() {
+  $('pre code').each(function() {
+    $(this).addClass("hljs");
+  });
 
-}
+});;
